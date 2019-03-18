8a95dda036c513e9dfc2593bb2c6af9ab1804951
Grant <grant@Grants-MacBook-Pro.local>
2019-03-18 12:29:27 +0000

    updated configuration files

--- src/main/java/com/mindera/login/controllers/UsersController.java
+++ src/main/java/com/mindera/login/controllers/UsersController.java
@@ -17,26 +17,31 @@ public class UsersController {
     @Autowired
     private UsersService usersService;
 
+    // GET http://localhost:8080/users
     @RequestMapping(method = GET)
     public List<User> getAllUsers() {
         return usersService.getAllUsers();
     }
 
+    // GET http://localhost:8080/users/1
     @RequestMapping(method = GET, path = "/{user-id}")
     public User getUserById(@PathVariable(name = "user-id") int userId) {
         return usersService.getUserById(userId);
     }
 
+    // POST http://localhost:8080/users
     @RequestMapping(method = POST)
     public User createNewUser(@RequestBody User user) {
         return usersService.createNewUser(user);
     }
 
+    // PUT http://localhost:8080/users/1
     @RequestMapping(method = PUT, path = "/{user-id}")
     public User updateUser(@PathVariable(name = "user-id") int userId, @RequestBody User user) {
         return usersService.updateUser(userId, user);
     }
 
+    // DELETE http://localhost:8080/users/1
     @RequestMapping(method = DELETE, path = "/{user-id}")
     public User deleteUser(@PathVariable(name = "user-id") int userId) {
         return usersService.deleteUser(userId);
